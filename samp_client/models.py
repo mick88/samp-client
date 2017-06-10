6bd31753653c470137c98838cd2f7c9dd0b4ee2a
michaldabski <contact@michaldabski.com>
2017-06-10 21:34:53 +0100

    Define models for rule and client responses

--- samp_client/models.py
+++ samp_client/models.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import, unicode_literals
 from collections import namedtuple
 
 ServerInfo = namedtuple('ServerInfo', ['password', 'players', 'max_players', 'hostname', 'gamemode', 'language'])
+Rule = namedtuple('Rule', ['name', 'value'])
+Client = namedtuple('Client', ['name', 'score'])
+ClientDetail = namedtuple('ClientDetail', ['id', 'name', 'score', 'ping'])
