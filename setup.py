990a42b16bf6793c5ae94e387990c23085c416d9
michaldabski <contact@michaldabski.com>
2017-06-11 23:08:30 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='samp-client',
+    version='1.0',
+    packages=['samp_client'],
+    url='http://git.michaldabski.com/website/samp-api',
+    license='MIT',
+    author='Michal Dabski',
+    author_email='contact@michaldabski.com',
+    description='SA-MP API client for python supporting both query and RCON APIs'
+)
