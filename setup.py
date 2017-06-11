a0e3667804e67b57a25654dbde2e16db796d10b3
michaldabski <contact@michaldabski.com>
2017-06-11 23:23:51 +0100

    Change url to github

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='samp-client',
     version='1.0',
     packages=['samp_client'],
-    url='http://git.michaldabski.com/website/samp-api',
+    url='https://github.com/mick88/samp-client',
     license='MIT',
     author='Michal Dabski',
     author_email='contact@michaldabski.com',
