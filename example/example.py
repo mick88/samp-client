439cf4456ccef47f8bba447e55779df4e2056604
michaldabski <contact@michaldabski.com>
2017-06-11 11:42:06 +0100

    Revert changes to example script

--- example/example.py
+++ example/example.py
@@ -2,4 +2,23 @@ from __future__ import unicode_literals, absolute_import
 from samp_client.client import SampClient
 
 with SampClient(address='localhost', rcon_password='123') as client:
-    print client.rcon_get_language()
+    server_info = client.get_server_info()
+    print repr(server_info)
+
+    print 'Rules:'
+    for rule in client.get_server_rules():
+        print '{}={}'.format(
+            rule.name,
+            rule.value,
+        )
+
+    print 'Connected players:'
+    for player in client.get_server_clients():
+        print '{} ({})'.format(player.name, player.score)
+
+
+    print 'Connected players (detailed):'
+    for player in client.get_server_clients_detailed():
+        print repr(player)
+
+    print client.send_rcon_command('varlist')
