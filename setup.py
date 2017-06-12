61131f286ace2afb7f84becb9bc4fbfbe1a8aa74
michaldabski <contact@michaldabski.com>
2017-06-12 20:36:06 +0100

    Get version number from git tags

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
+import os
 from distutils.core import setup
+
+
+def get_version():
+    import subprocess
+    version = subprocess.check_output('git describe --always --tags'.split(' '), cwd=os.path.dirname(__file__) or None).strip()
+    version = version.split('-')
+    if len(version) == 1:
+        return version[0]
+    else:
+        return '{0}.dev{1}'.format(*version)
+
 
 setup(
     name='samp-client',
-    version='1.0',
+    version=get_version(),
     packages=['samp_client'],
     url='https://github.com/mick88/samp-client',
     license='MIT',
