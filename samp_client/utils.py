7aa9fcbe6969c23243d4ea576d8f98c0fedac855
michaldabski <contact@michaldabski.com>
2017-06-12 19:51:09 +0100

    Cast strings to native str class

--- samp_client/utils.py
+++ samp_client/utils.py
@@ -57,7 +57,7 @@ def decode_string(string, len_pos, len_bytes=4):
     assert isinstance(len_pos, int)
     len_end = len_pos + len_bytes
     length = decode_int(string[len_pos:len_end])
-    return string[len_end:len_end + length]
+    return str(string[len_end:len_end + length])
 
 
 def build_rcon_command(command, args=None):
