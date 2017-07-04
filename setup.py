86647ae7e189a38e9d7d67048c639ce2c3074b08
michaldabski <contact@michaldabski.com>
2017-07-04 20:17:33 +0100

    Hardcode version number

--- setup.py
+++ setup.py
@@ -1,21 +1,10 @@
 #!/usr/bin/env python
-import os
 from distutils.core import setup
-
-
-def get_version():
-    import subprocess
-    version = subprocess.check_output('git describe --always --tags'.split(' '), cwd=os.path.dirname(__file__) or None).strip()
-    version = version.split('-')
-    if len(version) == 1:
-        return version[0]
-    else:
-        return '{0}.dev{1}'.format(*version)
 
 
 setup(
     name='samp-client',
-    version=get_version(),
+    version='2.0.1',
     packages=['samp_client'],
     url='https://github.com/mick88/samp-client',
     license='MIT',
