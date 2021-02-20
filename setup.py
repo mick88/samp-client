beebe9dc4d8ddc836bf3d3061f2e83d79afebcf5
Michal Dabski <michaldabski@gmail.com>
2021-02-20 12:08:35 +0000

    Release 3.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except:
 
 setup(
     name='samp-client',
-    version='3.0',
+    version='3.0.0',
     packages=['samp_client'],
     url='https://github.com/mick88/samp-client',
     license='MIT',
